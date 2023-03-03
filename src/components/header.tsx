2e9caa6e696bb6fe0048faccce31d2b5f390b8a3
mogoatlhe <iam@ofent.se>
2023-03-03 12:03:05 +0200

    add nav component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import Nav from "./nav";
 
 const Header = ({ title }: { title: string }) => {
   return (
-    <Head>
-      <title>{title}</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+    </>
   );
 };
 
---
+++ src/components/nav.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <Image src="./../../public/images/logo.png" alt="halffl logo" />
+        <p>halffl</p>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
