d4488eb9bc9b9f4958b78db469991098badfabd2
mogoatlhe <iam@ofent.se>
2023-03-03 14:20:22 +0200

    add nav component

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
+import logo from "./../../public/images/logo.png";
+import { IBM_Plex_Serif } from "next/font/google";
+
+const ibm_plex_serif = IBM_Plex_Serif({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const Nav = () => {
   return (
     <>
-      <nav>
-        <Image src="./../../public/images/logo.png" alt="halffl logo" />
-        <p>halffl</p>
+      <nav className="h-15 flex items-end gap-1 border-b border-zinc-900 bg-gradient-to-r from-[#be9a1c]/[.4] to-[#1E1E1E] p-3">
+        <div className="relative h-8 w-8">
+          <Image
+            src={logo}
+            alt="halffl logo"
+            fill
+            sizes="(max-width: 768px) 100px,
+              (max-width: 1200px) 50vw,
+              33vw"
+          />
+        </div>
+        <p className={`${ibm_plex_serif.className} text-sm`}>halffl</p>
       </nav>
     </>
   );
