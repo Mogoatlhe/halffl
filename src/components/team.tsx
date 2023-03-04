1071cec5daa561cb5cd949c629a96e8b8ace4e6c
mogoatlhe <iam@ofent.se>
2023-03-04 22:03:21 +0200

    remove image container

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -4,21 +4,19 @@ import type Team from "~/types/Team";
 const Team_Container = ({ team, index }: { team: Team; index: number }) => {
   return (
     <div className={`grid w-full grid-cols-8 gap-2 p-2 text-sm`}>
-      <div className="col-span-3 flex items-center gap-2 whitespace-nowrap">
+      <div className="col-span-3 flex items-center gap-2">
         <p>{`${index}`}</p>
-        <div className="h-12/12 relative w-3/12">
-          <Image
-            alt="Next.js logo"
-            src={team.logo}
-            width={24}
-            height={24}
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-            }}
-          />
-        </div>
-        {/* <p>{`${team.name}`}</p> */}
+        <Image
+          alt="Next.js logo"
+          src={team.logo}
+          width={24}
+          height={24}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        />
+        <p className="w-4 whitespace-nowrap">{`${team.name}`}</p>
       </div>
       <span>{team.times_played}</span>
       <span>{team.wins}</span>
