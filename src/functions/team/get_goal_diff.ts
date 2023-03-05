826dea0c808e7720d5b47ec48d51ef05c73f51dc
mogoatlhe <iam@ofent.se>
2023-03-05 21:33:39 +0200

    fix goal diff calculation

--- src/functions/team/get_goal_diff.ts
+++ src/functions/team/get_goal_diff.ts
@@ -1,14 +1,10 @@
 import type Score from "~/types/Score";
 
 const get_goal_diff = (ground: string, score: Score) => {
-  const goal_diff =
-    score.halftime.home -
-    score.halftime.away +
-    score.fulltime.home -
-    score.fulltime.away;
+  const goal_diff = score.fulltime.home - score.fulltime.away;
 
   if (ground === "home") return goal_diff;
-  return -goal_diff;
+  return -1 * goal_diff;
 };
 
 export default get_goal_diff;
