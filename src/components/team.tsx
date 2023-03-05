dc379b41fe93d83c8de9d17969b4fdabe3a43a5b
mogoatlhe <iam@ofent.se>
2023-03-05 10:08:08 +0200

    truncate overlapping team names

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -3,20 +3,22 @@ import type Team from "~/types/Team";
 
 const Team_Container = ({ team, index }: { team: Team; index: number }) => {
   return (
-    <div className={`grid w-full grid-cols-8 gap-2 p-2 text-sm`}>
+    <div className={`grid w-full grid-cols-8 gap-2 px-1 py-2 text-sm`}>
       <div className="col-span-3 flex items-center gap-2">
-        <p>{`${index}`}</p>
-        <Image
-          alt="Next.js logo"
-          src={team.logo}
-          width={24}
-          height={24}
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-        <p className="w-4 whitespace-nowrap">{`${team.name}`}</p>
+        <div className="flex">
+          <p className="flex w-10 justify-center">{`${index}`}</p>
+          <Image
+            alt="Next.js logo"
+            src={team.logo}
+            width={24}
+            height={24}
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+        </div>
+        <p className="w-16 truncate whitespace-nowrap">{`${team.name}`}</p>
       </div>
       <span>{team.times_played}</span>
       <span>{team.wins}</span>
