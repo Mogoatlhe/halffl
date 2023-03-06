8b5e7616b7da55c220409307ebacbc3bdf95c522
mogoatlhe <iam@ofent.se>
2023-03-06 10:22:28 +0200

    create responsive design for laptops and >

--- src/components/league.tsx
+++ src/components/league.tsx
@@ -23,9 +23,18 @@ const Leagues_Container = ({
     </option>
   ));
 
+  const get_league_buttons = leagues.map((league) => (
+    <button
+      key={league.id}
+      className="w-52 border border-r-0 border-amber-800 p-4 font-bold shadow-lg"
+    >
+      {league.name}
+    </button>
+  ));
+
   return (
     <>
-      <div className="flex h-12 flex-col justify-center border-b border-zinc-400 p-3 sm:h-16">
+      <div className="flex h-12 flex-col justify-center border-b border-zinc-400 p-3 sm:h-16 lg:mt-6 lg:h-5/6 lg:w-72  lg:border lg:pr-0">
         <label
           className={`fonts-extrabold text-xs sm:text-sm ${inria_sans.className}`}
           htmlFor="leagues"
@@ -35,11 +44,14 @@ const Leagues_Container = ({
         <select
           value={current_league}
           id="leagues"
-          className={`w-44 bg-white pl-3 text-sm sm:w-52 sm:text-base ${iceland.className} cursor-pointer`}
+          className={`w-44 bg-white pl-3 text-sm sm:w-52 sm:text-base ${iceland.className} cursor-pointer lg:hidden`}
           onChange={handleChange}
         >
           {get_league_options}
         </select>
+        <div className="hidden w-full gap-4 pt-4 text-sm lg:flex lg:flex-col lg:items-end">
+          {get_league_buttons}
+        </div>
       </div>
     </>
   );
