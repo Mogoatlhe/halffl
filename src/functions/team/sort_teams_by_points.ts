50a3e87ae25eb977d3b3c581c6756938d30dc5a8
mogoatlhe <iam@ofent.se>
2023-03-07 19:52:29 +0200

    make ""sort teams by points" function pure

--- src/functions/team/sort_teams_by_points.ts
+++ src/functions/team/sort_teams_by_points.ts
@@ -1,7 +1,8 @@
 import type Team from "~/types/Team";
 
 const sort_teams_by_points = (teams: Team[]) => {
-  teams.sort((team_1, team_2) => {
+  const temp_teams = JSON.parse(JSON.stringify(teams)) as Team[];
+  temp_teams.sort((team_1, team_2) => {
     if (team_1.points > team_2.points) return -1;
 
     if (team_1.points < team_2.points) return 1;
@@ -12,6 +13,8 @@ const sort_teams_by_points = (teams: Team[]) => {
 
     return 0;
   });
+
+  return temp_teams;
 };
 
 export default sort_teams_by_points;
