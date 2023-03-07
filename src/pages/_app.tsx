5ebe5905094853a47381b4b99ea502a4572744ef
mogoatlhe <iam@ofent.se>
2023-03-07 11:34:37 +0200

    add audience analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
-import { type AppType } from "next/dist/shared/lib/utils";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
 
 export default MyApp;
