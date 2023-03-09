4388a6d1c3bceffcfb14b16696d266e23bd2d162
mogoatlhe <iam@ofent.se>
2023-03-10 00:07:43 +0200

    fetch data using next revalidate

--- src/functions/request/get_requests.ts
+++ src/functions/request/get_requests.ts
@@ -1,8 +1,14 @@
 const get_requests = (urls: string[]) => {
+  const max_requests_per_second = 864; // seconds in a day / allowed requests per day ( 86400 / 100 )
+  const max_batch_requests_per_second = max_requests_per_second * urls.length;
+
   return urls.map((url) =>
     fetch(url, {
       headers: {
         "x-rapidapi-key": process.env.FOOTBALL_API_KEY as string,
+      },
+      next: {
+        revalidate: max_batch_requests_per_second,
       },
     }).then((response) => response.json())
   );
