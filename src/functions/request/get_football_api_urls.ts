aae8764e6df6d533633dcac4de9a0560f9634065
mogoatlhe <iam@ofent.se>
2024-05-06 23:30:09 +0200

    update urls: football api -> rapidapi

--- src/functions/request/get_football_api_urls.ts
+++ src/functions/request/get_football_api_urls.ts
@@ -1,14 +1,14 @@
 import get_current_date from "../general/get_current_date";
 
 const get_football_api_urls = () => {
-  const start_year = 2022;
-  const from_date = "2022-07-01";
+  const start_year = 2023;
+  const from_date = "2023-07-01";
   const league_ids = [288, 39, 140, 61, 135, 78, 88, 94];
   const current_date = get_current_date();
 
   const urls = league_ids.map(
     (league_id) =>
-      `https://v3.football.api-sports.io/fixtures?league=${league_id}&season=${start_year}&to=${current_date}&from=${from_date}`
+      `https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${league_id}&season=${start_year}&from=${from_date}&to=${current_date}`
   );
 
   return urls;
