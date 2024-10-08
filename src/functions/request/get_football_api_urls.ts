f88cb60f56b627abc4ad2e58598d6644419b72cb
Ofentse Moeketsi Mogoatlhe <iam@ofent.se>
2024-10-08 07:46:05 +0200

    Update get_football_api_urls.ts
    update the start date to 2024

--- src/functions/request/get_football_api_urls.ts
+++ src/functions/request/get_football_api_urls.ts
@@ -1,8 +1,8 @@
 import get_current_date from "../general/get_current_date";
 
 const get_football_api_urls = () => {
-  const start_year = 2023;
-  const from_date = "2023-07-01";
+  const start_year = 2024;
+  const from_date = "2024-07-01";
   const league_ids = [288, 39, 140, 61, 135, 78, 88, 94];
   const current_date = get_current_date();
 
