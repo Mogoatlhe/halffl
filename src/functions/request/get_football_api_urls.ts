5c9bd7d28496243ddcb70f97ac6a573a04d7e627
mogoatlhe <u14254922@tuks.co.za>
2025-11-11 18:59:46 +0200

    automate date resetting for new seasons

--- src/functions/request/get_football_api_urls.ts
+++ src/functions/request/get_football_api_urls.ts
@@ -1,10 +1,15 @@
 import get_current_date from "../general/get_current_date";
 
 const get_football_api_urls = () => {
-  const start_year = 2024;
-  const from_date = "2024-07-01";
-  const league_ids = [288, 39, 140, 61, 135, 78, 88, 94];
+  const now = new Date();
+  const start_year = now.getFullYear();
   const current_date = get_current_date();
+  const league_ids = [288, 39, 140, 61, 135, 78, 88, 94];
+
+  // automatically resets date every year
+  // but has no data between `year-07-01` and start of league
+  const from_date = new Date(`${start_year}-07-01`) > new Date(current_date) 
+    ? `${start_year - 1}-07-01` : `${start_year}-07-01`;
 
   const urls = league_ids.map(
     (league_id) =>
